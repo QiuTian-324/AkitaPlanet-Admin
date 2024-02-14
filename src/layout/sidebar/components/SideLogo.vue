08bcbda96bcaed07d9cb8c1f718d8da83fee8be3
QiuTian-324 <1240092443@qq.com>
2024-02-14 11:42:09 +0800

    :sparkles:feat: 侧边栏组件

---
+++ src/layout/sidebar/components/SideLogo.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useThemeStore } from '@/store'
+
+const themeStore = useThemeStore()
+const title = import.meta.env.VITE_TITLE
+</script>
+
+<template>
+  <RouterLink class="h-[60px] flex items-center justify-center" to="/">
+    <div class="h-full flex items-center justify-center">
+      <img src="/image/logo.svg" alt="logo" class="h-[36px] w-[36px]">
+    </div>
+    <h2
+      v-show="!themeStore.collapsed"
+      class="ml-2 max-w-[150px] flex-shrink-0 text-base text-primary font-bold dark:text-white"
+    >
+      {{ title }}
+    </h2>
+  </RouterLink>
+</template>
---
+++ src/layout/sidebar/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SideLogo from './components/SideLogo.vue'
+import SideMenu from './components/SideMenu.vue'
+</script>
+
+<template>
+  <!-- 图标 -->
+  <SideLogo />
+  <!-- 侧边菜单 -->
+  <SideMenu />
+</template>
